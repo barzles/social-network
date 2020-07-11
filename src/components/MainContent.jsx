40ce3b554894175e0135a78a9dc842c579953d4c
barzles <lesbarz97@gmail.com>
2020-07-11 19:30:34 +0300

    first commit:

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MainContent = () => {
+    return (
+        <main className='content'>
+            <div className="content-background"></div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>1 post</div>
+                    <div>2 post</div>
+                    <div>3 post</div>
+                </div>
+            </div>
+        </main>
+    )
+};
+
+export default MainContent;
